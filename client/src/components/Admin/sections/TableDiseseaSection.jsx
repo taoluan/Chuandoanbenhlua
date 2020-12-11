c7da40103720859a7cb9d66db839a76bc037c199
Tao Luan <tvanluanst@gmail.com>
2020-12-12 01:41:11 +0700

    day-31-react

--- client/src/components/Admin/sections/TableDiseseaSection.jsx
+++ client/src/components/Admin/sections/TableDiseseaSection.jsx
@@ -1,4 +1,4 @@
-import { MDBCard, MDBCardBody, MDBDataTableV5, MDBTableBody, MDBTableHead, MDBRow, MDBCol ,MDBCardHeader,MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
+import { MDBCard, MDBCardBody, MDBDataTableV5, MDBTableBody, MDBTableHead, MDBRow, MDBCol ,MDBCardHeader,MDBTable, MDBBtn,MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
 import React,{useEffect,useState} from 'react';
 import diseseaApi from '../../../api/diseseaApi'
 
@@ -22,30 +22,42 @@ const TableDiseseaSection = () => {
         ],
         rows:[]
         })
+    const [checkbox1, setCheckbox1] = useState('');
+    const [data,setdata] = useState([])
+    const [show, setshow] = useState(false);
+    const fetchDanhSachBenh = async()=>{
+        const respose = await diseseaApi.dsBenhNoType()
+        let newArr = []
+        respose.map(x=>{
+        x.benh.map(y=>{
+            newArr.push({ten_benh: y.ten_benh , ten_loaibenh: x.loaibenh})
+        })
+        })
+        setDatatable({
+        ...datatable,
+        rows:newArr
+        })
+    }
+    const fetchThongTinBenh= async(benh)=>{
+        const respose = await diseseaApi.getThongTinBenh({uri_benh: benh})
+        let newArr = []
+        newArr = Object.values(respose)
+        return newArr
+    }
     useEffect(() => {
-        const fetchDanhSachBenh = async()=>{
-            const respose = await diseseaApi.dsBenhNoType()
-            let newArr = []
-            respose.map(x=>{
-            x.benh.map(y=>{
-                newArr.push({ten_benh: y.ten_benh , ten_loaibenh: x.loaibenh})
-            })
-            })
-            setDatatable({
-            ...datatable,
-            rows:newArr
-            })
-        }
         fetchDanhSachBenh()
         },[]);
-    const showLogs2 = (e) => {
+    const showLogs2 = async (e) => {
         setCheckbox1(e.ten_benh);
-
+        setshow(true)
+        let rs = await fetchThongTinBenh(e.ten_benh)
+        setdata(rs)
+        console.log(data)
         };
         return(
-            (datatable.rows.length >0)
+            (datatable.rows.length >0 )
                 ?   (
-                    <MDBRow className="justify-content-center mb-5 mt-4">
+                    <MDBRow className="justify-content-start mb-5 mt-4">
                         <MDBCol sm="5">
                             <MDBCard>
                             <MDBCardHeader className="text-center title-2 mt-0 mb-0 text-dark">Danh sách bệnh</MDBCardHeader>
@@ -66,26 +78,38 @@ const TableDiseseaSection = () => {
                                 </MDBCardBody>
                             </MDBCard>
                         </MDBCol>
-                        <MDBCol sm="7">
-                            <MDBCard>
-                            <MDBCardHeader className="text-center title-2 mt-0 mb-0 text-dark">Danh sách bệnh</MDBCardHeader>
-                                <MDBCardBody>
-                                <MDBDataTableV5
-                                    hover
-                                    entriesOptions={[12, 20, 25]}
-                                    entries={12}
-                                    pagesAmount={4}
-                                    data={datatable}
-                                    checkbox
-                                    headCheckboxID='id2'
-                                    bodyCheckboxID='checkboxes2'
-                                    getValueCheckBox={(e) => {
-                                    showLogs2(e);
-                                    }}
-                                />
-                                </MDBCardBody>
-                            </MDBCard>
-                        </MDBCol>
+                            {data.length > 0
+                                && <MDBCol sm="7" style={{ display: show ? "block" : "none" }}>
+                                    <MDBCard className="z-depth-0 m-0 p-0">
+                                        <MDBCardBody className="p-0 ">
+                                            <MDBTable >
+                                                    <MDBTableHead color="dark" >
+                                                    <tr>
+                                                        <th className="text-center" style={{width:'20%'}}>Giới thiệu</th>
+                                                        <th className="text-center">Mô tả</th>
+                                                    </tr>
+                                                    </MDBTableHead>
+                                                    <MDBTableBody>
+                                                        {
+                                                            data.map((item,key)=>{
+                                                               return( 
+                                                                   (item.type)
+                                                                        && (
+                                                                            <tr key={key}>
+                                                                                <td>{item.type}</td>
+                                                                                <td>{item.value} <img style={{cursor: 'pointer'}} onClick={()=>{alert(item.uri)}} src={process.env.PUBLIC_URL + '/img/pen.png'} width="20" height="20" alt=""/></td>
+                                                                            </tr>
+                                                                        )
+                                                               )
+                                                            })
+                                                        }
+                                                   
+                                                    </MDBTableBody>
+                                            </MDBTable>
+                                        </MDBCardBody>
+                                    </MDBCard>
+                                </MDBCol>
+                                }
                     </MDBRow>
                     )
                 : (
